d9f2e32b50773f6bfa206de5f6484c8608cad7f4
Ahmed Ghonim <ahmed.ghonim.dev@gmail.com>
2023-09-15 01:50:21 +0300

    fix: 🎨 dd

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   server: {
-    port: 80,
+    port: 3000,
     proxy: {
       "/api": {
         target,
