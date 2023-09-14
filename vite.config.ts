3eff80898addf867be1117e6d022ac3ad21639d6
Ahmed Ghonim <ahmed.ghonim.dev@gmail.com>
2023-09-15 02:24:01 +0300

    feat: 🐛 add api

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const target = "https://personaltrainerkmm.com/api/admin";
+// const target = "https://personaltrainerkmm.com/api/admin";
 // https://vitejs.dev/config/
 export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   server: {
     port: 3000,
-    proxy: {
-      "/api": {
-        target,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target,
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ""),
+    //   },
+    // },
   },
   build: {
     outDir: "build",
