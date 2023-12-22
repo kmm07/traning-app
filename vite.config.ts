230fd15c40c37560915e216ae02759faa3ee47f4
Joe <eljoe1717@gmail.com>
2023-12-22 14:33:05 +0200

    Fix Trans Fat

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   server: {
     port: 3000,
-    watch: {
-      usePolling: true
-    }
   },
   build: {
     outDir: "build",
   },
-  plugins: [tsconfigPaths(), react({
-    include: "**/*.tsx",
-  })],
+  plugins: [tsconfigPaths(), react()],
 });
