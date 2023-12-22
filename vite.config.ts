9033ca9c87a91c85897ee17f6aebd6d5534b85b7
Joe <eljoe1717@gmail.com>
2023-12-23 01:04:18 +0200

    Fix Trans Fat

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   server: {
     port: 3000,
+    target: 'https://thirsty-franklin.85-215-43-232.plesk.page',
+    changeOrigin: true,
+    rewrite: (path) => path.replace(/^\/api/, ''),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      // Add other CORS headers as needed
+    },
   },
   build: {
     outDir: "build",
