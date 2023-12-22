e5757c19d453e39a49a4bcfd56745035786ba52f
Ahmed Ghonim <35805998+ahmedghonim@users.noreply.github.com>
2023-12-23 01:31:15 +0200

    Update proxy path for admin API

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
+      '/api/admin': {
         target: 'https://thirsty-franklin.85-215-43-232.plesk.page',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api\/admin/, ''),
       },
     },
   },
