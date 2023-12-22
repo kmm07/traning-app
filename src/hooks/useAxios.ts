ba771268f948710a6cbb965901af1fe72255bdd6
Joe <eljoe1717@gmail.com>
2023-12-23 01:52:11 +0200

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -16,10 +16,12 @@ const useAxios = ({ contentType = "application/json" }: Props) => {
     useAppSelector(selectCurrentToken);
 
   return axios.create({
+    baseURL: "/api/admin",
     headers: {
       "Content-Type": contentType as string,
       accept: "application/json",
       authorization: `Bearer ${access_token as string}`,
+      'Access-Control-Allow-Origin': '*'
     },
   });
 };
