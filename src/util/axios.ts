8a48b364e3b851be7ac776c1062389e8ec05e58c
Ahmed Ghonim <35805998+ahmedghonim@users.noreply.github.com>
2023-12-23 01:10:20 +0200

    Remove unnecessary imports and update API endpoints

--- src/util/axios.ts
+++ src/util/axios.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const customAxios = () =>
   axios.create({
-    baseURL: "https://thirsty-franklin.85-215-43-232.plesk.page/api/admin",
     headers: {
       "Content-Type": "application/json",
       accept: "application/json",
