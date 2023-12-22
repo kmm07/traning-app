21beecf458c5f1c45417dac4b8e2adf189d64b03
Joe <eljoe1717@gmail.com>
2023-12-23 01:59:36 +0200

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api/admin': {
-        target: 'https://thirsty-franklin.85-215-43-232.plesk.page/api/admin',
+      '/': {
+        target: 'https://thirsty-franklin.85-215-43-232.plesk.page',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/admin/, ''),
+        // rewrite: (path) => path.replace(/^\/api\/admin/, ''),
         
       },
     },
