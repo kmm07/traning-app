e713b9e2ccb4658c8256424696e6bcf6a57d18a3
Joe <eljoe1717@gmail.com>
2023-12-27 15:19:57 +0200

    Fix

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -16,7 +16,7 @@ const useAxios = ({ contentType = "application/json" }: Props) => {
     useAppSelector(selectCurrentToken);
 
   return axios.create({
-    baseURL: "/api/admin",
+    baseURL: "http://personaltrainerkmm.com/api/admin",
     headers: {
       "Content-Type": contentType as string,
       accept: "application/json",
