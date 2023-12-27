357890375fb091d917cd073728c13b7134ea14da
Joe <eljoe1717@gmail.com>
2023-12-27 15:21:28 +0200

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   server: {
     port: 3000,
-    proxy: {
+    // proxy: {
     //   '/api/admin': {
     //     target: 'http://personaltrainerkmm.com/api/admin/',
     //     changeOrigin: true,
